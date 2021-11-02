9cba0986205017cb6d10a9589c229730986860de
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-11-02 15:07:09 +0100

    fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,51 @@
 export default {
-  target: 'static', // default is 'server'
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  target: "static",
   head: {
-    title: 'martin-kohnle',
+    title: "martin-kohnle",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     css: [
       // Load a Node.js module directly (here it's a Sass file)
-      'bulma',
+      "bulma",
       // CSS file in the project
-      '~/assets/css/main.css',
+      "~/assets/css/main.css",
       // SCSS file in the project
-      '~/assets/css/main.scss'
+      "~/assets/css/main.scss"
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/main.scss'
-  ],
+  css: ["@/assets/css/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxt/image',
-  ],
+  buildModules: ["@nuxt/image"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    "@nuxt/content"
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
+  build: {},
+  generate: {
+    fallback: true
   }
-}
+};
