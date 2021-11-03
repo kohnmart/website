8ec492a58a7b129cc2be5a06b324e4c46cf62c58
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-11-03 18:11:05 +0100

    header, image, css fix

--- components/Header.vue
+++ components/Header.vue
@@ -2,22 +2,26 @@
   <div class="header">
     <ul>
       <li style="float:left"><nuxt-link to="/">Martin Kohnle</nuxt-link></li>
-      <li><nuxt-link to="/projects">Projects</nuxt-link></li>
-      <li>
-        <a href="mailto: martin.kohnle@outlook.de">
-          <i class="far fa-envelope"></i
-        ></a>
-      </li>
-      <li>
-        <a href="https://github.com/kohnmart/">
-          <i class="fab fa-github"></i
-        ></a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/martin-kohnle-504b2b1ba/">
-          <i class="fab fa-linkedin"></i>
-        </a>
-      </li>
+      <div>
+        <li style="float:left">
+          <nuxt-link to="/projects">Projects</nuxt-link>
+        </li>
+        <li>
+          <a href="mailto: martin.kohnle@outlook.de">
+            <i class="far fa-envelope"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://github.com/kohnmart/">
+            <i class="fab fa-github"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/martin-kohnle-504b2b1ba/">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </li>
+      </div>
     </ul>
   </div>
 </template>
@@ -33,12 +37,9 @@
     margin: 20px;
     padding: 0;
     overflow: hidden;
-
     li {
       float: right;
       a {
-        display: block;
-        text-align: center;
         padding: 10px 10px;
         text-decoration: none;
         font-weight: 300;
@@ -53,14 +54,13 @@
 }
 @media (max-width: 600px) {
   .header {
-    margin-top: 2%;
-    margin-bottom: 2%;
+    margin-top: 10%;
+    margin-bottom: 10%;
     ul {
       list-style-type: none;
       padding: 0;
       overflow: hidden;
       li {
-   
         a {
           padding: 5px 8px;
           text-decoration: none;
@@ -69,7 +69,6 @@
         }
         .far,
         .fab {
-          font-size: 0.6em;
           display: none;
         }
       }
