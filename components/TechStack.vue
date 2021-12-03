0b3a94bc03d645dd4ec5cc353cc224759cefc825
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-03 16:05:36 +0100

    Create tech-stack component

---
+++ components/TechStack.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="stack" v-for="item in stack" :key="item">
+      <li>{{ item }}</li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    stack: Array
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+    .stack {
+        display: inline-block;
+        margin-bottom: 2em;
+        li {
+            list-style: none;
+            margin-right: 0.5em;
+            font-size: 1em;
+            border: 2px solid black;
+            color: white;
+            padding: 5px 15px;
+            border-radius: 5px;
+        }   
+    }
+</style>>
